f478e0a0accd7d3547a54ff47947d1a1554ef0f8
kamrulhasan3412 <kamrulhasan.devcoder@gmail.com>
2025-12-10 16:10:21 +0600

    added navbar with responsive

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../../components/Header/Navbar";
+// import Banner from "../../components/Banner/Banner";
+
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
